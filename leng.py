9f2bdbdc8f34543b073473568938fa7cb3e57779
monumk <73218094+monumk@users.noreply.github.com>
2021-06-26 23:16:07 +0530

    Add files via upload

---
+++ leng.py
@@ -0,0 +1,12 @@
+'''Count the total number of characters in a string'''
+
+str1='DAV Institute of Management'
+print(len(str1))
+#output 27
+
+
+str2='DAV CENTENARY COLLEGE'
+print(len(str2))
+#output 27
+
+
